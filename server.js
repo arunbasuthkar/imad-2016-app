6a8948fa02c5bb25e6e54d0c49d3ebdd71eabd89
arunbasuthkar <arunbasuthkar009@gmail.com>
2016-09-22 19:30:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,19 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-app.get('/article-two',function(req,res){
-    res.send('article two will be serverd');
-});
-    app.get('/article-three',function(req,res){
-    res.send('article three will be serverd');
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
